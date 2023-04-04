22f5ff0ae6938a0cc91748d412086494c86725cc
solarpark7346 <solarpark7346@gmail.com>
2023-04-04 20:44:10 +0900

    update: Metadata 객체 값 추가

--- src/main/java/com/develop/web/video/Metadata.java
+++ src/main/java/com/develop/web/video/Metadata.java
@@ -7,7 +7,9 @@ public class Metadata {
     public String filename;
     public int width;
     public int height;
+    public String format_name;
     public String format_long_name;
+    public String tags;
     public double duration;
     public Long size;
 }
