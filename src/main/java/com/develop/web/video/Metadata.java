e88db61a6fb075ac14be7d8b8ce7cc282f7fc1c4
solarpark7346 <solarpark7346@gmail.com>
2023-04-04 15:58:14 +0900

    add: metadata vo 추가

---
+++ src/main/java/com/develop/web/video/Metadata.java
@@ -0,0 +1,13 @@
+package com.develop.web.video;
+
+import lombok.Data;
+
+@Data
+public class Metadata {
+    public String filename;
+    public int width;
+    public int height;
+    public String format_long_name;
+    public double duration;
+    public Long size;
+}
