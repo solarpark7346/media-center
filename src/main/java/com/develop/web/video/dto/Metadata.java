d4ff2507c1ab22c01a5c56f7dc0365842f409002
solarpark7346 <solarpark7346@gmail.com>
2023-04-19 16:48:10 +0900

    update: id, ext 추가

--- src/main/java/com/develop/web/video/dto/Metadata.java
+++ src/main/java/com/develop/web/video/dto/Metadata.java
@@ -4,7 +4,9 @@ import lombok.Data;
 
 @Data
 public class Metadata {
+    public String id;
     public String filename;
+    public String ext;
     public int width;
     public int height;
     public String format_name;
@@ -12,4 +14,18 @@ public class Metadata {
     public String tags;
     public double duration;
     public Long size;
+
+    public String toString(){
+        return "\n[ Metadata ]\n" +
+                "id = " + id + "\n" +
+                "filename = " + filename + "\n" +
+                "ext = " + ext + "\n" +
+                "width = " + width + "\n" +
+                "height = " + height + "\n" +
+                "format_name = " + format_name + "\n" +
+                "format_long_name = " + format_long_name + "\n" +
+                "tags = " + tags + "\n" +
+                "duration = " + duration + "\n" +
+                "size = " + size + "\n";
+    }
 }
